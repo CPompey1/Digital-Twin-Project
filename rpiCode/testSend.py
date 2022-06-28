2db76fbf48db9d7f4ab9b890b785a6b66a182a25
CPompey1 <cpomey10@gmail.com>
2022-06-28 09:56:06 -0400

    Updated testSend

--- rpiCode/testSend.py
+++ rpiCode/testSend.py
@@ -4,6 +4,14 @@ import json
 import serial
 from websocket import create_connection
 
+#This Script sends a command through serial to the external device
+#to generate "dummy data". The external device then sends this dummy data
+#in a formatted data block. The script gets the "temperature" in the formated
+#and formats it into a dictionary. The script then converts this string to
+#a formatted json string and sends it to AWS. The script then prints the
+#server response.
+
+
 ws = create_connection("wss://cvyykl1zo6.execute-api.us-east-1.amazonaws.com/prod",header={'x-api-key':'zQXx6dS25g1osj74gXAL51nAmdAOBANL2gAvR3O8'})
 
 
@@ -20,7 +28,7 @@ ser.flushInput()
 bufferIn = ser.read_until(size = 2)
 out = {
     "action" : "upload_data",
-    "epoch_time": (int(time.time() * 1000),
+    "epoch_time": (int(time.time() * 1000)),
     "data": {
         "temperature" : {
             "degrees_celsius" : int(bufferIn[1])
